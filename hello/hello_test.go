0b8c38162f02341ccda43470839396c3fdac5cb1
Siddharth Dhulipalla <sdhulipalla@palantir.com>
2019-07-14 08:23:57 -0700

    adding integers

---
+++ hello/hello_test.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jacques", "French")
+		want := "Bonjour, Jacques"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Hindi", func(t *testing.T) {
+		got := Hello("Sid", "Hindi")
+		want := "Namaste, Sid"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
