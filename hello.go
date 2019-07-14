de9529f4b704ba09a5e916dd9af375a6ec8ac03b
Siddharth Dhulipalla <sdhulipalla@palantir.com>
2019-07-14 08:17:02 -0700

    adding hindi and refactoring more

--- hello.go
+++ hello.go
@@ -4,17 +4,32 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const hindiHelloPrefix = "Namaste, "
 const spanish = "Spanish"
+const french = "French"
+const hindi = "Hindi"
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case hindi:
+		prefix = hindiHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
